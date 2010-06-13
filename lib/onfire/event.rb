5dcf7631ed86a6c08158f9ea5de09242858067ef
niklas <niklas@brueckenschlaeger.de>
2010-06-13 16:23:40 +0200

    more informative Event#to_s/#inspect

--- lib/onfire/event.rb
+++ lib/onfire/event.rb
@@ -2,24 +2,24 @@ module Onfire
   # An Event is born in #fire and is passed up the ancestor chain of the triggering datastructure.
   # It carries a <tt>type</tt>, the fireing widget <tt>source</tt> and arbitrary payload <tt>data</tt>.
   class Event
-    
+
     attr_accessor :type, :source, :data
-    
+
     def initialize(type=nil, source=nil, data=nil)
       @type       = type
       @source     = source
       @data       = data
     end
-    
+
     def stopped?
       @stopped ||= false
     end
-    
+
     # Stop event bubbling.
     def stop!
       @stopped = true
     end
-    
+
     ### FIXME: what about serialization? should we simply forget the source?
     def _dump(depth)
       ""
@@ -27,5 +27,9 @@ module Onfire
     def self._load(str)
       ::Onfire::Event.new
     end
+
+    def to_s
+      "#<Onfire::Event:0x#{object_id.to_s(16)} type: #{type} source:#{source} data: #{data}>"
+    end ; alias :to_s :inspect
   end
 end
