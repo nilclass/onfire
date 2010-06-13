7baaae4007f5777551c66ef0fc4bd13b38eb7d33
niklas <niklas@brueckenschlaeger.de>
2010-06-13 15:58:29 +0200

    added default handler support: * use Onfire#set_default_handler and pass it either a block or a symbol referencing a method on self. * that handler gets called for every event that has *no* handler associated on the current object. * tests included.

--- lib/onfire.rb
+++ lib/onfire.rb
@@ -6,45 +6,51 @@ module Onfire
     table_options = {}
     table_options[:event_type]  = event_type
     table_options[:source_name] = options[:from] if options[:from]
-    
+
     if block_given?
       return attach_event_handler(block, table_options)
     end
-    
+
     attach_event_handler(options[:do], table_options)
   end
-  
+
   def fire(event_type)
     bubble_event Event.new(event_type, self)
   end
-  
+
   def bubble_event(event)
     process_event(event) # locally process event, then climb up.
     return if root?
-    
+
     parent.bubble_event(event)
   end
-  
+
   def process_event(event)
-    local_event_handlers(event).each do |proc|
+    handlers = local_event_handlers(event)
+    handlers.any? ? handlers.each do |proc|
       return if event.stopped?
       proc.call(event)
-    end
+    end : (@__default_event_handler__ && @__default_event_handler__.call(event))
   end
-  
+
   def root?
     !parent
   end
-  
+
   def event_table
     @event_table ||= Onfire::EventTable.new
   end
-    
+
+  def set_default_event_handler(method=nil, &block)
+    block = lambda { |evt| self.send(method, evt) } unless block_given?
+    @__default_event_handler__ = block
+  end
+
   protected
     def attach_event_handler(proc, table_options)
       event_table.add_handler(proc, table_options)
     end
-    
+
     # Get all handlers from self for the passed event.
     def local_event_handlers(event)
       event_table.all_handlers_for(event.type, event.source)
--- test/onfire_test.rb
+++ test/onfire_test.rb
@@ -6,16 +6,16 @@ class OnfireTest < Test::Unit::TestCase
       attr_accessor :list
       attr_accessor :name
       attr_accessor :parent
-      
+
       include Onfire
-      
+
       def initialize(name)
         @name = name
         @list = []
       end
     end.new(name)
   end
-  
+
   context "including Onfire" do
     should "provide event_table accessors to an emtpy table" do
       table = mock.event_table
@@ -23,85 +23,108 @@ class OnfireTest < Test::Unit::TestCase
       assert_equal 0, table.size
     end
   end
-  
+
   context "#process_event" do
     setup do
       @event = Onfire::Event.new(:click, mock)
     end
-    
-    
+
+
     should "not fill #list as there are no event handlers attached" do
       obj = mock
       obj.process_event(@event)
-      
+
       assert_equal [], obj.list
     end
-    
+
     should "invoke exactly one proc and thus push `1` onto #list" do
       obj = mock
       obj.event_table.add_handler(lambda { |evt| obj.list << 1 }, :event_type => :click)
-      
+
       obj.process_event(@event)
-      
+
       assert_equal [1], obj.list
     end
-    
+
     should "not invoke procs for another event_type" do
       obj = mock
       obj.event_table.add_handler(lambda { |evt| obj.list << 1 }, :event_type => :click)
       obj.event_table.add_handler(lambda { |evt| obj.list << 2 }, :event_type => :drop) # don't call me!
-      
+
       obj.process_event(@event)
-      
+
       assert_equal [1], obj.list
     end
+
+    context "with default handler" do
+      setup {
+        @obj = mock
+        @obj.set_default_event_handler do |event|
+          @default_event_handler_called = true
+        end
+      }
+      context 'with no handler for a given event' do
+        setup { @obj.process_event(@event) }
+        should('call the default handler') { assert @default_event_handler_called }
+      end
+
+      context 'with handler for a given event' do
+        setup {
+          @obj.event_table.add_handler(lambda { |evt| @specific_event_handler_called = true }, :event_type => :click)
+          @obj.process_event(@event)
+        }
+
+        should('call that specific handler')   { assert @specific_event_handler_called }
+        should('not call the default handler') { assert ! @default_event_handler_called }
+      end
+    end
   end
-  
+
   context "calling #on" do
     setup do
       @obj    = mock
       @event  = Onfire::Event.new(:click, @obj)
     end
-    
+
     context "with a block" do
       should "add a handler to the event_table when called with a block" do
         @obj.on :click do
           @obj.list << 1
         end
-        
+
         @obj.process_event(@event)
         assert_equal [1], @obj.list
       end
-      
+
       should "invoke two handlers if called twice" do
         @obj.on :click do @obj.list << 1 end
         @obj.on :click do @obj.list << 2 end
-        
+
         @obj.process_event(@event)
         assert_equal [1,2], @obj.list
       end
-      
+
       should "receive the triggering event as parameter" do
         @obj.on :click do |evt|
           @obj.list << evt
         end
-        
+
         @obj.process_event(@event)
         assert_equal [@event], @obj.list
       end
     end
   end
-  
+
   context "In the bar" do
     setup do
       @barkeeper   = mock('barkeeper')
       @nice_guest  = mock('nice guest')
       @bad_guest   = mock('bad guest')
-      
+
       @nice_guest.parent = @barkeeper
       @bad_guest.parent  = @barkeeper
     end
-    
+
     context "the #on method" do
       context "with the :from option for filtering" do
         setup do
@@ -109,29 +132,29 @@ class OnfireTest < Test::Unit::TestCase
           @barkeeper.on(:order, :from => @bad_guest)  {@barkeeper.list << 'ignore'}
           @barkeeper.on(:order, :from => @bad_guest)  {@barkeeper.list << 'throw out'}
         end
-      
+
         should "invoke the handler for the nice guest only" do
           @nice_guest.fire :order
           assert_equal ['be nice'], @barkeeper.list
         end
-        
+
         should "invoke both handlers for the bad guest only" do
           @bad_guest.fire :order
           assert_equal ['ignore', 'throw out'], @barkeeper.list
         end
-        
+
         should "invoke an additional catch-all handler" do
           @barkeeper.on(:order) {@barkeeper.list << 'have a drink yourself'}
           @nice_guest.fire :order
           assert_equal ['be nice', 'have a drink yourself'], @barkeeper.list
         end
-        
+
         should "invoke another handler when :from is nil" do
           @barkeeper.on(:order, :from => nil) {@barkeeper.list << 'have a drink yourself'}
           @nice_guest.fire :order
           assert_equal ['be nice', 'have a drink yourself'], @barkeeper.list
         end
-        
+
         should "invoke :from handlers before it processes catch-all handlers" do
           @barkeeper.on(:order)                         {@barkeeper.list << 'have a drink yourself'}
           @barkeeper.on(:order, :from => @nice_guest)   {@barkeeper.list << 'bring out toast'}
@@ -139,7 +162,7 @@ class OnfireTest < Test::Unit::TestCase
           assert_equal ['be nice', 'bring out toast', 'have a drink yourself'], @barkeeper.list
         end
       end
-      
+
       context "with a callable object" do
         setup do
           @callable = Class.new.new
@@ -151,20 +174,20 @@ class OnfireTest < Test::Unit::TestCase
             end
           end
         end
-        
+
         should "add a handler to the local event_table" do
           @barkeeper.on :order, :do => @callable
-          
+
           @barkeeper.fire :order
           assert_equal ['order from barkeeper'], @barkeeper.list
-          
+
           @nice_guest.fire :order
           assert_equal ['order from barkeeper', 'order from guest'], @barkeeper.list
         end
       end
     end
-    
-    
+
+
     context "stopping events" do
       should "not invoke any handler after the guest kills it" do
         @nice_guest.on(:order) {@nice_guest.list << 'thirsty?'}
@@ -174,51 +197,51 @@ class OnfireTest < Test::Unit::TestCase
         end
         @barkeeper.on(:order)  {@nice_guest.list << 'draw a beer'}
         @nice_guest.fire :order
-        
+
         assert_equal ['thirsty?', 'money?'], @nice_guest.list
       end
     end
-    
-    
+
+
     context "#event_table" do
       should "expose the EventTable to the public" do
         assert_kind_of ::Onfire::EventTable, @barkeeper.event_table
       end
     end
-    
+
   end
-  
+
   context "calling #fire" do
     setup do
       @obj    = mock
     end
-    
+
     should "be of no relevance when there are no handlers attached" do
       @obj.fire :click
-      
+
       assert_equal [], @obj.list
     end
-    
+
     should "invoke the attached matching handler" do
       @obj.on :click do @obj.list << 1 end
       @obj.fire :click
-      
+
       assert_equal [1], @obj.list
     end
-    
+
     should "not invoke same handlers for :symbol or 'string' event names" do
       @obj.on :click do @obj.list << 1 end
       @obj.fire 'click'
-      
+
       assert_equal [], @obj.list
     end
-    
-    
+
+
     should "invoke handlers in the correct order when bubbling" do
       # we use @obj for recording the chat.
       bar   = mock('bar')
       guest = mock('guest')
-      
+
       guest.parent = bar
       guest.on :thirsty do
         @obj.list << 'A beer!'
@@ -233,28 +256,28 @@ class OnfireTest < Test::Unit::TestCase
       bar.on :order do
         @obj.list << 'There you go.'
       end
-      
+
       guest.fire :thirsty
-      
+
       assert_equal ['A beer!', 'There you go.', 'Hurry up, man!', 'Thanks.'], @obj.list
     end
-    
+
   end
-  
-  
+
+
   context "#root?" do
     setup do
       @obj    = mock
     end
-    
+
     should "return false if we got parents" do
       @obj.parent = :daddy
       assert !@obj.root?
     end
-    
+
     should "return true if we're at the top" do
       assert @obj.root?
     end
   end
-  
+
 end
